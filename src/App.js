5d19097212a06f1e297d700ee9a51faa15fb0aff
Joan Andrés <andres6936@live.com>
2020-07-16 09:01:54 -0500

    Added form-inline

--- src/App.js
+++ src/App.js
@@ -5,8 +5,47 @@ export default class App extends Component {
 
     render() {
         return (
-            <div className="mx-auto">
-                Hello
+            <div className="container-fluid">
+                <div className="col-lg-8 offset-lg-2">
+                    <div className="row row-cols-2">
+                        <div className="col">
+                            <header>
+                                <h1>Artists</h1>
+                            </header>
+
+                            <section className="row">
+                                <form className="col-6 form-inline">
+                                    <label>Categories</label>
+                                    <select className="form-control">
+                                        <option>Bachata</option>
+                                        <option>Salsa</option>
+                                        <option>Rock</option>
+                                    </select>
+                                </form>
+
+                                <form className="col-6 form-inline">
+                                    <label>Artists</label>
+                                    <select className="form-control">
+                                        <option>AC/DC</option>
+                                        <option>Romeo</option>
+                                        <option>J. Balvin</option>
+                                    </select>
+                                </form>
+                            </section>
+                            <section>
+
+                            </section>
+                            <section>
+
+                            </section>
+                        </div>
+                        <div className="col">
+                            <header>
+                                <h1>Sings</h1>
+                            </header>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
