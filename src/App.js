01ab5b313455fcaf722007bf9c4c84f1ad39e433
Joan Andrés <andres6936@live.com>
2020-07-16 10:56:38 -0500

    Create new class for manage the inferior panel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {artists} from './artists';
 import {PanelSuperior} from "./karaoke/PanelSuperior";
+import {PanelInferior} from "./karaoke/PanelInferior";
 
 export default class App extends Component {
 
@@ -14,25 +15,7 @@ export default class App extends Component {
             <div className="container-fluid">
                 <div className="col-lg-8 offset-lg-2 mt-5">
                     <PanelSuperior data={this.data}/>
-
-                    <div className="row row-cols-2">
-                        <section className="col col-8">
-                            <header className="col-12">
-                                <h4 className="text-muted">List of Play</h4>
-                            </header>
-                        </section>
-                        <section className="col col-4">
-                            <header className="col-12">
-                                <h4 className="text-muted">Options List of Play</h4>
-                            </header>
-
-                            <div className="btn-group col-12">
-                                <button className="btn btn-primary">Add List</button>
-                                <button className="btn btn-primary">Modify List</button>
-                                <button className="btn btn-danger">Delete List</button>
-                            </div>
-                        </section>
-                    </div>
+                    <PanelInferior/>
                 </div>
             </div>
         );
---
+++ src/karaoke/PanelInferior.js
@@ -0,0 +1,25 @@
+import React, {Component} from "react";
+
+export class PanelInferior extends Component {
+
+    render() {
+        return <div className="row row-cols-2">
+            <section className="col col-8">
+                <header className="col-12">
+                    <h4 className="text-muted">List of Play</h4>
+                </header>
+            </section>
+            <section className="col col-4">
+                <header className="col-12">
+                    <h4 className="text-muted">Options List of Play</h4>
+                </header>
+
+                <div className="btn-group col-12">
+                    <button className="btn btn-primary">Add List</button>
+                    <button className="btn btn-primary">Modify List</button>
+                    <button className="btn btn-danger">Delete List</button>
+                </div>
+            </section>
+        </div>
+    }
+}
