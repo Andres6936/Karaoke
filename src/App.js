5dc6f4cccf799672553ce53c52eda0b30efff2fb
Joan Andrés <andres6936@live.com>
2020-07-23 12:34:06 -0500

    Change the responsibility of show information of artists and tracks

--- src/App.js
+++ src/App.js
@@ -4,18 +4,70 @@ import {PanelSuperior} from "./karaoke/PanelSuperior";
 import {PanelInferior} from "./karaoke/PanelInferior";
 import {PanelOptions} from "./karaoke/PanelOptions";
 
+/**
+ * @property data {[{img: string, name: string, tracks:
+ *      [{duration: number, difficulty: number, name: string, lyrics: string}]
+ *  }]}
+ */
 export default class App extends Component {
 
     constructor(props) {
         super(props);
+
         this.data = artists;
+        this.state = {
+            currentArtist : 0,
+            currentTrack: 0,
+        }
     }
 
+    /**
+     * @returns {[string]} List with the names of artists.
+     */
+    filterArtists(){
+        let artists = [];
+        this.data.forEach(artist => {
+            artists.push(artist.name);
+        })
+        return artists;
+    }
+
+    /**
+     * @returns {[string]} List with the name of current artist's track.
+     */
+    filterTracks(){
+        let tracks = [];
+        this.data[this.state.currentArtist].tracks.forEach(track =>
+            tracks.push(track.name));
+        return tracks;
+    }
+
+    /**
+     * @returns {string} Path to resource that represent the image
+     *  of current artist.
+     */
+    getPathImage(){
+        return this.data[this.state.currentArtist].img;
+    }
+
+    /**
+     * @returns {{duration: number, difficulty: number, name: string, lyrics: string}}
+     *  Information of current track based in the current artist.
+     */
+    getInfoTrack(){
+        return {
+            name: this.data[this.state.currentArtist].tracks[this.state.currentTrack].name,
+            duration: this.data[this.state.currentArtist].tracks[this.state.currentTrack].duration,
+            difficulty: this.data[this.state.currentArtist].tracks[this.state.currentTrack].difficulty,
+            lyrics: this.data[this.state.currentArtist].tracks[this.state.currentTrack].lyrics,
+        }
+    }
+    
     render() {
         return (
             <div className="container-fluid">
                 <div className="col-lg-6 offset-lg-3 mt-5">
-                    <PanelSuperior data={this.data}/>
+                    <PanelSuperior artists={this.filterArtists()} tracks={this.filterTracks()} image={this.getPathImage()} track={this.getInfoTrack()}/>
                     <PanelInferior/>
                     <PanelOptions/>
                 </div>
--- src/karaoke/PanelSuperior.js
+++ src/karaoke/PanelSuperior.js
@@ -4,63 +4,12 @@ import {PanelTrack} from "./PanelTrack";
 
 export class PanelSuperior extends Component {
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            currentArtist : 0,
-            currentTrack: 0,
-        }
-    }
-
-    /**
-     * @returns {[string]} List with the names of artists.
-     */
-    filterArtists(){
-        let artists = [];
-        this.props.data.forEach(artist => {
-            artists.push(artist.name);
-        })
-        return artists;
-    }
-
-    /**
-     * @returns {[string]} List with the name of current artist's track.
-     */
-    filterTracks(){
-        let tracks = [];
-        this.props.data[this.state.currentArtist].tracks.forEach(track =>
-            tracks.push(track.name));
-        return tracks;
-    }
-
-    /**
-     * @returns {string} Path to resource that represent the image
-     *  of current artist.
-     */
-    getPathImage(){
-        return this.props.data[this.state.currentArtist].img;
-    }
-
-    /**
-     * @returns {{duration: number, difficulty: number, name: string, lyrics: string}}
-     *  Information of current track based in the current artist.
-     */
-    getInfoTrack(){
-        return {
-            name: this.props.data[this.state.currentArtist].tracks[this.state.currentTrack].name,
-            duration: this.props.data[this.state.currentArtist].tracks[this.state.currentTrack].duration,
-            difficulty: this.props.data[this.state.currentArtist].tracks[this.state.currentTrack].difficulty,
-            lyrics: this.props.data[this.state.currentArtist].tracks[this.state.currentTrack].lyrics,
-        }
-    }
-
     render() {
         return <div className="row row-cols-2">
-            <PanelArtist artists={this.filterArtists()}
-                         tracks={this.filterTracks()}
-                         image={this.getPathImage()}/>
-            <PanelTrack track={this.getInfoTrack()}/>
+            <PanelArtist artists={this.props.artists}
+                         tracks={this.props.tracks}
+                         image={this.props.image}/>
+            <PanelTrack track={this.props.track}/>
         </div>
     }
 }
