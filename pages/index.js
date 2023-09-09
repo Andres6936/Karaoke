c5cb7e75186ce9122581ba1d0030a804feeab407
Andres6936 <andres6936@gmail.com>
2023-09-09 09:47:06 -0500

    Added Typescript and MasterCSS

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import {Component} from 'react';
 import {artists} from './artists';
 import {PanelSuperior} from "./karaoke/PanelSuperior";
